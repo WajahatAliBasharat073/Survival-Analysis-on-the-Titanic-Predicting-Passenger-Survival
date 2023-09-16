5762f9888a81f621f794da6363dfb3de72af9847
Wajahat Ali Basharat <83087637+WajahatAliBasharat073@users.noreply.github.com>
2023-09-16 19:12:17 +0500

    Folder structure push again

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict if a passenger survived the sinking of the Titanic or not.',
+    author='Wajahat Ali Basharat',
+    license='',
+)
